8fc04e881496b20eab7e319629aa57cff582f262
Gazdag András <andras.gazdag@prosupport.io>
2022-09-15 20:56:13 +0200

    Add README, Team ID as parameter

--- time_logger_from_csv.py
+++ time_logger_from_csv.py
@@ -2,33 +2,35 @@ import os
 import csv
 import time
 import datetime
+import json
 from urllib import request, parse
 from dotenv import load_dotenv
 
 load_dotenv()
 
 CLICKUP_USER_ID = os.getenv('CLICKUP_USER_ID')
+CLICKUP_TEAM_ID = os.getenv('CLICKUP_TEAM_ID')
 CLICKUP_ACCESS_TOKEN = os.getenv('CLICKUP_ACCESS_TOKEN')
 
 with open("./times.csv", 'r') as file:
   csvreader = csv.reader(file, delimiter=';')
   for row in csvreader:
-    taskId = row[0]
-    dateTime = row[1]
-    durationInMin = row[2]
-    desc = row[3]
+    dateTime = row[0]
+    durationInMin = row[1]
+    desc = row[2]
+    taskId = row[3]
 
     duration = int(durationInMin) * 60 * 1000
 
     date_time = datetime.datetime.strptime(dateTime, "%Y-%m-%d %H:%M:%S")
-    unixTime = time.mktime(date_time.timetuple())
+    unixTime = int(time.mktime(date_time.timetuple()) * 1000)
 
     print(taskId, dateTime, durationInMin, desc)
 
     values = {
         "description": desc,
-        "start": 1655881788000,
-        "billable": true,
+        "start": unixTime,
+        "billable": "true",
         "duration": duration,
         "assignee": int(CLICKUP_USER_ID),
         "tid": taskId
@@ -41,9 +43,9 @@ with open("./times.csv", 'r') as file:
 
     print(values)
 
-    postData = parse.urlencode(values).encode()
+    postData = json.dumps(values).encode('utf8')
 
-    req = request.Request('https://api.clickup.com/api/v2/team/2545761/time_entries/?custom_task_ids=&team_id=', data=postData, headers=headers)
+    req = request.Request('https://api.clickup.com/api/v2/team/' + CLICKUP_TEAM_ID + '/time_entries/', data=postData, headers=headers)
 
     response_body = request.urlopen(req).read()
     print(response_body)
